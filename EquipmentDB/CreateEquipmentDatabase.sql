be673f02d3ab18445baac122c230a43f12b90e40
Thom Barrett <thom440@yahoo.com>
2021-01-11 14:56:11 -0800

    Added database script

--- EquipmentDB/CreateEquipmentDatabase.sql
+++ EquipmentDB/CreateEquipmentDatabase.sql
@@ -1,1 +1,9 @@
-﻿CREATE DATABASE ComputerEquipment
+﻿
+CREATE DATABASE ComputerEquipment
+
+CREATE TABLE Equipment
+(
+	EquipmentID			int						PRIMARY KEY IDENTITY
+	, Name					varchar(100)		NOT NULL
+	, Price					smallmoney		NOT NULL
+)
